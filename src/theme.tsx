20a42e89e8d7cddb2fc9a869bdbebaae29dd9af4
yifan.yuan <yifan.yuan@linecorp.com>
2022-03-01 22:48:13 +0900

    Move from https://github.com/TsingJyujing/synclip

---
+++ src/theme.tsx
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+// A custom theme for this app
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#CCCCFF"
+        }
+    },
+});
+
+export default theme;
