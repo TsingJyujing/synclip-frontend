2043d08daea038aa045d86aa89f8388a6db4f622
yifan.yuan <yifan.yuan@linecorp.com>
2022-03-12 16:21:20 +0900

    Add remove one history

--- src/component/LocalClipboardHistory.tsx
+++ src/component/LocalClipboardHistory.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import { Counter } from "util/Utils";
 import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
 import i18n from "i18n";
 import HistoryIcon from '@mui/icons-material/History';
-import ContentPasteGoIcon from '@mui/icons-material/ContentPasteGo';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 export const LocalClipboardHistory = () => {
     const { t } = i18n;
     const [cacheId, setCacheId] = React.useState(0);
@@ -22,13 +23,29 @@ export const LocalClipboardHistory = () => {
                 if (k !== null) {
                     const v = window.localStorage.getItem(k);
                     return (
-                        <ListItemButton component="a" href={`/clipboard/${k}`}>
-                            <ListItemIcon><ContentPasteGoIcon color="success" /></ListItemIcon>
-                            <ListItemText
-                                primary={v || t("No Name")}
-                                secondary={k}
-                            />
-                        </ListItemButton>
+                        <ListItem
+                            secondaryAction={
+                                <IconButton edge="end" aria-label="delete" onClick={() => {
+                                    if (window.confirm(t("do you want to remove this item from history list?"))) {
+                                        window.localStorage.removeItem(k);
+                                        setCacheId(cacheId + 1);
+                                    }
+                                }}>
+                                    <DeleteIcon color="error" />
+                                </IconButton>
+                            }
+                        >
+                            <ListItemButton
+                                component="a"
+                                href={`/clipboard/${k}`}
+                            >
+                                <ListItemText
+                                    primary={v || t("No Name")}
+                                    secondary={k}
+                                />
+                            </ListItemButton>
+                        </ListItem>
+
                     );
                 } else {
                     return undefined;
